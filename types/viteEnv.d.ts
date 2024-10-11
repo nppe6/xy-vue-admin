365ba04dddc8a2041bb2f7c40f29094262aa9a1e
nppe6 <xiao2313667401@foxmail.com>
2024-10-11 11:10:53 +0800

    初步完成对env配置文件的类型声明以及类型转换，以及抽离vite.config.ts配置文件的抽离

---
+++ types/viteEnv.d.ts
@@ -0,0 +1,6 @@
+interface ViteEnv {
+  VITE_ROUTER_AUTOLOAD: boolean
+  VITE_API_URL: string
+}
+
+
--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,27 @@
-import { defineConfig } from 'vite'
+import { ConfigEnv, defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import { resolve } from 'path'
+import alias from './vite/alias'
+import { parseEnv } from './vite/utils'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()],
-  resolve: {
-    alias: {
-      '@': resolve(__dirname, 'src')
+// export default defineConfig({
+//   plugins: [vue()],
+//   resolve: {
+//     alias
+//   }
+// })
+
+// command 表示当前Vite执行的命令 mode 是一个字符串，表示当前Vite的运行模式
+export default ({ command, mode }: ConfigEnv) => {
+  const isBuild = command == 'build' // 当前执行的 命令是什么
+  const root = process.cwd() // 当前项目文件路径
+  const env = parseEnv(loadEnv(mode,root))
+  
+
+  return {
+    plugins: [vue()],
+    resolve: {
+      alias
     }
   }
-})
+}
