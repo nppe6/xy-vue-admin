ff7b9be14945dabd084635a21b954396fe7ca60b
nppe6 <xiao2313667401@foxmail.com>
2024-10-14 23:13:45 +0800

    登录页布局完成

--- src/components/form/xy-input.vue
+++ src/components/form/xy-input.vue
@@ -8,7 +8,6 @@
 
 <style lang="scss" scoped>
 input{
-  @apply border w-full px-3 py-2 outline-none border-gray-200 placeholder:text-xs 
-  focus:ring-2 ring-offset-2 ring-violet-300 duration-300 focus:border-white
+  @apply  w-full px-4 py-2 outline-none border-gray-200 placeholder:text-xs 
 }
 </style>
--- src/views/auth/login.vue
+++ src/views/auth/login.vue
@@ -1,6 +1,13 @@
 <template>
-  <div>
-    login.vue
+  <div class="login-content">
+    <div class="form">
+      <h1>通用后台脚手架</h1>
+      <div class="form-content">
+        <xy-input class=" bg-transparent text-white bg-[#283443]" placeholder="请输入手机号或邮箱账号" type="text"></xy-input>
+        <xy-input class=" bg-transparent text-white bg-[#283443]" placeholder="请输入密码" type="password"></xy-input>
+        <xy-button>登录</xy-button>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -8,6 +15,20 @@
 
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
+.login-content{
+  @apply h-screen bg-[#2d3a4b] flex justify-center items-center p-5 md:p-0;
 
+  .form{
+    @apply w-[420px] -translate-y-32;
+
+    h1{
+      @apply text-white text-2xl text-center font-semibold mb-8
+    }
+
+    .form-content{
+      @apply w-full flex flex-col gap-4;
+    }
+  }
+}
 </style>
