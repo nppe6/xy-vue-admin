03d5981267d1267d0f1a5cfe4f2534647289f8a8
nppe6 <xiao2313667401@foxmail.com>
2024-10-20 23:30:09 +0800

    菜单组件单独抽离并进一步完善逻辑代码

--- src/layouts/components/menu.vue
+++ src/layouts/components/menu.vue
@@ -1,61 +1,122 @@
 <template>
   <div class="menu xy-bg-primary">
-    <div class="logo ">
-      <i class="fab fa-bilibili "></i>
+    <div class="logo">
+      <i class="fab fa-bilibili"></i>
       <span>日益努力</span>
     </div>
     <!-- 菜单 -->
-     <div class="left-container">
-      <dl>
-        <dt>
+    <div class="left-container">
+      <dl
+        v-for="(menu, index) of menus"
+        :key="index">
+        <dt @click="handleClick(menu)">
           <section>
-            <i class="fas fa-house-user mr-2"></i>
-            <span>错误页面</span>
+            <i
+              :class="menu.icon"
+              class="mr-2"></i>
+            <span>{{ menu.title }}</span>
           </section>
           <section>
             <i class="fas fa-angle-down text-md"></i>
           </section>
         </dt>
-        <dd class="active">404页面</dd>
-        <dd>403页面</dd>
+        <dd
+          v-show="menu.active"
+          :class="{ active: child.active }"
+          v-for="(child, index) of menu.children"
+          :key="index">
+          {{ child.title }}
+        </dd>
       </dl>
-     </div>
+    </div>
   </div>
 </template>
 
 <script setup lang="ts">
+import { reactive } from 'vue';
 
+interface IMenuItem {
+  title: string
+  icon?: string
+  active?: boolean
+}
+
+interface IMenu extends IMenuItem {
+  children: IMenuItem[]
+}
+
+const menus = reactive<IMenu[]>([
+  {
+    title: '错误页面',
+    icon: 'fas fa-house-user',
+    active: true,
+    children: [
+      {
+        title: '404页面',
+        active: true
+      },
+      {
+        title: '403页面'
+      }
+    ]
+  },
+  {
+    title: '编辑器',
+    icon: 'fas fa-house-user',
+    children: [
+      {
+        title: 'markdown文本器',
+        active: true
+      },
+      {
+        title: '富文本编辑器'
+      }
+    ]
+  }
+])
+
+const resetMenus = ()=>{
+  menus.forEach(pmenu => {
+    pmenu.active = false
+    pmenu.children?.forEach(cmenu => cmenu.active = false)
+  })
+}
+
+const handleClick = (pmenu: IMenuItem, cmenu: IMenuItem)=>{
+  resetMenus()
+  pmenu.active = true
+}
 </script>
 
 <style lang="scss" scoped>
-.menu{
+.menu {
   @apply w-[220px];
 
-  .logo{
+  .logo {
     @apply flex justify-center items-center p-4;
 
-    i{
+    i {
       @apply text-sky-500 text-4xl mr-4;
     }
-    span{
+    span {
       @apply text-gray-200 text-lg font-mono;
     }
   }
 
-  .left-container{
+  .left-container {
     @apply px-4 cursor-pointer;
 
-    dl{
-      @apply text-gray-300 test-sm;
+    dl {
+      @apply text-gray-300 text-sm;
 
-      dt{
+      dt {
         @apply text-lg mt-6 flex justify-between items-center;
       }
 
-      dd{
+      dd {
         @apply py-2 pl-4 my-2 text-white rounded-md hover:bg-sky-600 duration-300;
 
-        &.active{
+        &.active {
           @apply bg-sky-700;
         }
       }
